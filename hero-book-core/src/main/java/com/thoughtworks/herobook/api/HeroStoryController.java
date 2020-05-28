fbd80fc8ac84c2801f331436d4e2e30713462082
HeZhong <zhong.he@thoughtworks.com>
2020-05-28 17:33:44 +0800

    fix autowired warning.

--- hero-book-core/src/main/java/com/thoughtworks/herobook/api/HeroStoryController.java
+++ hero-book-core/src/main/java/com/thoughtworks/herobook/api/HeroStoryController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.herobook.api;
 
 import com.thoughtworks.herobook.dto.HeroStoryDTO;
 import com.thoughtworks.herobook.service.HeroStoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,9 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/hero-story")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class HeroStoryController {
-    @Autowired
-    HeroStoryService heroStoryService;
+
+    private final HeroStoryService heroStoryService;
 
     @GetMapping(params = "page")
     public Page<HeroStoryDTO> getHeroStoriesByPage(Pageable pageable) {
