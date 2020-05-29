e13b9b3a4f4468434cfc53b8581413c5e6e34558
Jie Peng <jie.peng@thoughtworks.com>
2020-05-29 11:39:41 +0800

    fix: add defalut value for timestamp

--- hero-book-core/src/main/resources/db/migration/V202005271853__create_table_hero_story.sql
+++ hero-book-core/src/main/resources/db/migration/V202005271853__create_table_hero_story.sql
@@ -3,6 +3,6 @@ CREATE TABLE `hero_story`(
     `title` varchar(200) NOT NULL,
     `author` varchar(50) NOT NULL,
     `content` LONG NOT NULL,
-    `created_time` timestamp,
-    `updated_time` timestamp
+    `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 )
