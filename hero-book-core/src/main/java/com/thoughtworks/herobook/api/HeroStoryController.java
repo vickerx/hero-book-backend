f94868ac45933d219923ee1a750d0a9804f357df
SongNan <nan.song@thoughtworks.com>
2020-06-01 11:44:36 +0800

    [Song]: add CrossOrigin.

--- hero-book-core/src/main/java/com/thoughtworks/herobook/api/HeroStoryController.java
+++ hero-book-core/src/main/java/com/thoughtworks/herobook/api/HeroStoryController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/hero-story")
+@CrossOrigin
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class HeroStoryController {
 
