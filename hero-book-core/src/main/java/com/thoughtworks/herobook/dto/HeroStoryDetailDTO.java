ba24300f05be835c201a2e68c0a10a5cdf2df33a
HeZhong <zhong.he@thoughtworks.com>
2020-06-01 15:42:22 +0800

    add getter and setter for hero story detail dto.

--- hero-book-core/src/main/java/com/thoughtworks/herobook/dto/HeroStoryDetailDTO.java
+++ hero-book-core/src/main/java/com/thoughtworks/herobook/dto/HeroStoryDetailDTO.java
@@ -1,7 +1,16 @@
 package com.thoughtworks.herobook.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class HeroStoryDetailDTO {
     private Long id;
 
