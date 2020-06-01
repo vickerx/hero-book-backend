6f5fe4c7068983022f85e31973559ddfd5c2f2f6
vickerx <vicker23@163.com>
2020-06-01 18:24:10 +0800

    Add url for Api

--- hero-book-gateway/src/main/java/com/thoughtworks/herobook/gateway/clients/UserApiClient.java
+++ hero-book-gateway/src/main/java/com/thoughtworks/herobook/gateway/clients/UserApiClient.java
@@ -2,8 +2,14 @@ package com.thoughtworks.herobook.gateway.clients;
 
 import com.thoughtworks.herobook.gateway.clients.dto.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "authservice")
+@RequestMapping("/user")
 public interface UserApiClient {
-    UserDto getUserByEmail(String email);
+
+    @GetMapping("/get-user-by-email")
+    UserDto getUserByEmail(@RequestParam("email") String email);
 }
