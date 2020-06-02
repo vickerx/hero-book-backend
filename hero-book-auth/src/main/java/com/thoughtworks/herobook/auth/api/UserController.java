9b25f998df6bce2a592373cf545e4793a72106ca
vickerx <vicker23@163.com>
2020-06-02 15:56:53 +0800

    Add get-by-email url for UserController

--- hero-book-auth/src/main/java/com/thoughtworks/herobook/auth/api/UserController.java
+++ hero-book-auth/src/main/java/com/thoughtworks/herobook/auth/api/UserController.java
@@ -1,21 +1,31 @@
 package com.thoughtworks.herobook.auth.api;
 
 import com.thoughtworks.herobook.auth.dto.UserDTO;
+import com.thoughtworks.herobook.auth.dto.response.UserResponse;
+import com.thoughtworks.herobook.auth.entity.User;
 import com.thoughtworks.herobook.auth.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("/user/registration")
+    @PostMapping("/registration")
     public ResponseEntity<Void> userRegistration(UserDTO userDTO) {
         userService.userRegistration(userDTO);
         return ResponseEntity.ok(null);
     }
 
+    @GetMapping("/get-by-email")
+    public UserResponse getByEmail(@RequestParam("email") String email) {
+        return UserResponse.of(userService.getByEmail(email));
+    }
 }
--- hero-book-auth/src/test/java/com/thoughtworks/herobook/auth/api/UserControllerTest.java
+++ hero-book-auth/src/test/java/com/thoughtworks/herobook/auth/api/UserControllerTest.java
@@ -1,11 +1,25 @@
 package com.thoughtworks.herobook.auth.api;
 
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockitoTestExecutionListener;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class UserControllerTest extends BaseControllerTest{
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        MockitoTestExecutionListener.class,
+        TransactionDbUnitTestExecutionListener.class})
+public class UserControllerTest extends BaseControllerTest {
 
     @Test
     void should_save_user_to_database() throws Exception {
@@ -14,5 +28,18 @@ public class UserControllerTest extends BaseControllerTest{
                 .param("password", "123456")
                 .param("email", "123@163.com"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @DatabaseSetup("/dbunit/UserControllerTest/should_return_user_when_get_by_email_setup.xml")
+    @DatabaseTearDown("/dbunit/UserControllerTest/user_clear_all.xml")
+    void should_return_user_when_get_by_email() throws Exception {
+        var email = "123@163.com";
+        mockMvc.perform(get("/user/get-by-email").param("email", email))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email", Matchers.is(email)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.username", Matchers.is("nick")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.password", Matchers.is("aaaaaaaa")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.isActivated", Matchers.is(true)));
     }
 }
