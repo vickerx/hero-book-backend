312b582455fd92d6d26070727f6c93c722fa748e
Jie Peng <jie.peng@thoughtworks.com>
2020-06-03 21:26:20 +0800

    add log when exception occured

--- hero-book-common/src/main/java/com/thoughtworks/herobook/common/advice/GlobalExceptionHandler.java
+++ hero-book-common/src/main/java/com/thoughtworks/herobook/common/advice/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.thoughtworks.herobook.common.advice;
 import com.thoughtworks.herobook.common.dto.ErrorResult;
 import com.thoughtworks.herobook.common.exception.BasicException;
 import com.thoughtworks.herobook.common.exception.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
@@ -13,10 +14,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = BindException.class)
     public ResponseEntity<ErrorResult> validateExceptionHandler(BindException bindException) {
+        log.error(bindException.getMessage(), bindException);
         String errorMessage = bindException.getAllErrors().stream()
                 .map(ObjectError::getDefaultMessage).collect(Collectors.joining("; "));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
@@ -25,12 +28,14 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = BasicException.class)
     public ResponseEntity<ErrorResult> errorHandler(BasicException basicException) {
+        log.error(basicException.getMessage(), basicException);
         return ResponseEntity.status(basicException.getHttpStatus())
                 .body(new ErrorResult(basicException.getErrorCode(), basicException.getMessage()));
     }
 
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ErrorResult> otherErrorsHandler(Exception exception) {
+        log.error(exception.getMessage(), exception);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ErrorResult(ErrorCode.HERO_BOOK_INTERNAL_ERROR, exception.getMessage()));
     }
