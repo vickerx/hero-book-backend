1999271c3bee5b6fb338c5c165bd67ea8599597e
vickerx <vicker23@163.com>
2020-06-05 12:24:33 +0800

    Add login failure

--- hero-book-gateway/src/main/java/com/thoughtworks/herobook/gateway/security/SecurityConfiguration.java
+++ hero-book-gateway/src/main/java/com/thoughtworks/herobook/gateway/security/SecurityConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
@@ -19,7 +18,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -38,6 +36,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .successHandler(((request, response, authentication) -> {
                     response.setContentType("application/json");
                     response.getWriter().write("{\"code\":0,\"msg\":\"success\"}");
+                    response.setStatus(HttpStatus.SC_OK);
+                }))
+                .failureHandler(((request, response, exception) -> {
+                    response.setContentType("application/json");
+                    response.getWriter().write("{\"code\":1,\"msg\":\"email or password error.\"}");
                     response.setStatus(HttpStatus.SC_OK);
                 }));
     }
--- hero-book-gateway/src/test/java/com/thoughtworks/herobook/gateway/security/LoginTest.java
+++ hero-book-gateway/src/test/java/com/thoughtworks/herobook/gateway/security/LoginTest.java
@@ -56,5 +56,16 @@ public class LoginTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code", Matchers.is(0)));
     }
 
+    @Test
+    public void should_login_failure_when_login_given_correct_email_and_error_password() throws Exception {
+        var user = UserResponse.builder().email("123@163.com").id(1L).isActivated(true).password("{MD5}{tyl4WmSKtePdnoq1m5eWo+E2NBeW0srEsmAbNo53y4g=}62b9ef3227a44f7a1a9297b65c0d33d4").build();
+        stubFor(get(urlPathEqualTo("/user/get-by-email"))
+                .willReturn(WireMock.aResponse().withBody(objectMapper.writeValueAsString(user)).withHeader("content-type", "application/json")));
+
+        mvc.perform(MockMvcRequestBuilders.post("/login").param("email", "123@163.com").param("password", "12345"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code", Matchers.is(1)));
+
+    }
 
 }
